f071e106386c60bc49edc78917b4ec2bd1dcad26
Odradex <sivakov.daniil@gmail.com>
2020-09-07 20:02:20 +0300

    Project created. 1st test.

---
+++ DiskExchange TG Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiskExchange_TG_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TEST github");
+        }
+    }
+}
