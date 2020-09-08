c5bc2bf37990bbd84514edf0736e6942bd9ecc99
Popkov Artem <artem891lol@gmail.com>
2020-09-08 19:19:14 +0300

    new menu button (goto)

--- DiskExchange TG Bot/Program.cs
+++ DiskExchange TG Bot/Program.cs
@@ -6,6 +6,8 @@ using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.InlineQueryResults;
 using System.Reflection;
 using Telegram.Bot.Args;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace DiskExchange_TG_Bot
 {
@@ -16,11 +18,37 @@ namespace DiskExchange_TG_Bot
         {
             bot = new TelegramBotClient("1299381797:AAF58uk3gqiSt9pkILwJ8970UXo2t_0_brQ") { Timeout = TimeSpan.FromSeconds(10)};
             bot.OnMessage += Bot_OnMessage;
-           
+           // bot.OnCallbackQuery += BotOnCallbackQueryReceived;
             bot.StartReceiving();
             Console.WriteLine($"Starting bot {bot.GetMeAsync().Result}...");
             Console.ReadKey();
         }
+
+        //private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs e)
+        //{
+        //    string buttonText = e.CallbackQuery.Data;
+        //    string name = $"{e.CallbackQuery.From.FirstName} {e.CallbackQuery.From.LastName}";
+        //    Console.WriteLine($"{name} нажал кнопку: '{buttonText}' ");
+        //    switch (buttonText)
+        //    {
+        //        case "Контакты":
+        //            var inlineKeyboard1 = new InlineKeyboardMarkup(new[]
+        //              {
+        //                new[]
+        //                {
+        //                    InlineKeyboardButton.WithUrl("Developer_1", "https://vk.com/blanker_bat"),
+        //                    InlineKeyboardButton.WithUrl("Developer_1", "https://vk.com/mr666tema666")
+
+        //                }
+        //            });
+        //            await bot.AnswerCallbackQueryAsync(e.CallbackQuery.Id,"Контакты");
+        //            //await bot.SendTextMessageAsync(e.CallbackQuery.Id, $"Контакты",
+        //                //replyMarkup: inlineKeyboard1);
+        //            break;
+        //            //await bot.SendTextMessageAsync(message.From.Id, "Выберите пунк меню",
+        //            //  replyMarkup: inlinekeyboard);
+        //    }
+        //}
 
         private static async void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
@@ -52,6 +80,7 @@ namespace DiskExchange_TG_Bot
                         replyMarkup: inlinekeyboard);
                     break;
                 case "/keyboard":
+                    startKeyboard:
                     var replyKeyboard = new ReplyKeyboardMarkup(new[]
                     {
                         new[]
@@ -61,11 +90,10 @@ namespace DiskExchange_TG_Bot
                         },
                         new[]
                         {
-                            //new KeyboardButton("Геолокация"){RequestLocation = true},
-                            //new KeyboardButton("Контакт"){RequestContact = true}
+                            
                             new KeyboardButton("Избранное"),
-                            new KeyboardButton("Помощь"),
-                            new KeyboardButton("Контакты")
+                            new KeyboardButton("Помощь")
+                            
                         }
 
                     }
@@ -74,6 +102,27 @@ namespace DiskExchange_TG_Bot
                     await bot.SendTextMessageAsync(message.Chat.Id, "Сообщение",
                         replyMarkup: replyKeyboard);
                   
+                    break;
+
+                case "Помощь":
+                    var replyKeyboard1 = new ReplyKeyboardMarkup(new[]
+                    {
+                        new[]
+                        {
+                             new KeyboardButton("Геолокация"){RequestLocation = true},
+                             new KeyboardButton("Контакт"){RequestContact = true},
+                             new KeyboardButton("Назад")
+
+
+
+                        }
+                    }
+                    );
+                    await bot.SendTextMessageAsync(message.Chat.Id, "Сообщение",
+                       replyMarkup: replyKeyboard1);
+                    break;
+                case "Назад":
+                    goto startKeyboard;
                     break;
                 case "/poll":
                     {
